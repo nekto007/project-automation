e40bf5a7cfd674d903a77889a1c582ea461ab86d
igorkorobko <nekto.korobko@gmail.com>
2023-10-20 14:01:23 +0300

    create user form and templates index and thanks

--- project_automation/urls.py
+++ project_automation/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+
+from projectautomation import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<int:user_id>', views.show_time_slots, name='choose_time'),
+    path('thanks/', views.show_thanks),
 ]
--- projectautomation/views.py
+++ projectautomation/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 
-# Create your views here.
+from .forms import ChooseTimeForm
+from .models import Student
+
+
+def show_time_slots(request, user_id):
+    try:
+        student = Student.objects.get(id=user_id)
+    except Student.DoesNotExist:
+        raise Http404('Page Not Found')
+    else:
+        if request.method == 'POST':
+            form = ChooseTimeForm(request.POST)
+            if form.is_valid():
+                best_time_slots = form.cleaned_data.get('best_time_slots')
+
+                student.best_time_slots.set(best_time_slots)
+
+
+                return HttpResponseRedirect('/thanks/')
+
+        form = ChooseTimeForm()
+        return render(request, 'index.html',
+                      context={
+                          'user_id': user_id,
+                          'form': form,
+                          'name': student.first_name
+                      })
+
+
+def show_thanks(request):
+    return render(request, 'thanks.html')
