fbe6d9c5d3a63fc671cb918208a3c9981c68acc8
igorkorobko <nekto.korobko@gmail.com>
2023-10-22 12:41:03 +0300

    refactoring show_time_slots

--- projectautomation/views.py
+++ projectautomation/views.py
@@ -1,45 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import ChooseTimeForm
-from .models import Student, Project, Group, PM, TimeSlot
+from .models import Group, PM, Project, Student, TimeSlot
 
 
 def show_time_slots(request, project_id, user_id):
-    try:
-        student = Student.objects.get(id=user_id)
-        project = Project.objects.get(id=project_id)
-    except Student.DoesNotExist or Project.DoesNotExist:
-        raise Http404('Page Not Found')
-    else:
-        if request.method == 'POST':
-            print('request.POST', request.POST)
-            form = ChooseTimeForm(request.POST)
-            print(form.fields)
-            print(form.errors)
-            if form.is_valid():
-                best_time_slots = form.cleaned_data.get('best_time_slots')
-                group = Group.objects.get(time_slot_id=best_time_slots)
-                print('find_group', group)
-                if group:
-                    group.students.add(student)
-                else:
-                    Group.objects.create(student)
-                return HttpResponseRedirect('/thanks/')
+    student = get_object_or_404(Student, id=user_id)
+    project = get_object_or_404(Project, id=project_id)
+    in_group = Group.objects.filter(project_id=project, students=student)
+    if in_group:
+        return HttpResponseRedirect('/thanks/')
+    if request.method == 'POST':
+        time_slot_id = request.POST.get('best_time_slots')
+        time_slot = get_object_or_404(TimeSlot, id=time_slot_id)
+        pm = get_object_or_404(PM, time_slots=time_slot_id)
 
-        time_slots = TimeSlot.objects.all()
-        best_time_slots = Group.objects.filter(is_complete=1).values("time_slot_id")
-        unavailable_slots = []
-        for slots in best_time_slots:
-            unavailable_slots.append(slots['time_slot_id'])
-        time_slots = TimeSlot.objects.exclude(id__in=unavailable_slots)
-        return render(request, 'index.html',
-                      context={
-                          'user_id': user_id,
-                          'project_id': project_id,
-                          'available_slots': time_slots,
-                          'name': student.first_name
-                      })
+        group, created = Group.objects.get_or_create(
+            time_slot=time_slot,
+            project=project,
+            defaults={
+                'title': f'{project.title} - {time_slot.start_time}',
+                'description': project.description,
+                'pm': pm,
+            }
+        )
+        group.students.add(student)
+
+        return redirect('/thanks/')
+
+    unavailable_slots = Group.objects.filter(is_complete=True).values_list('time_slot_id', flat=True)
+    available_slots = PM.objects.values_list('time_slots', flat=True)
+    time_slots = TimeSlot.objects.filter(id__in=available_slots) \
+        .exclude(id__in=unavailable_slots).values('id', 'start_time')
+
+    return render(request, 'index.html',
+                  context={
+                      'user_id': user_id,
+                      'project_id': project_id,
+                      'available_slots': time_slots,
+                      'name': student.first_name
+                  })
 
 
 def show_thanks(request):
