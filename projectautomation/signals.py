6c2c814a7ef199992d899d01303d0589fa0407e2
Vladimir <vladimir.papin@gmail.com>
2023-10-22 14:18:17 +0300

    change host

--- projectautomation/signals.py
+++ projectautomation/signals.py
@@ -5,8 +5,9 @@ from email.mime.text import MIMEText
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
-from .models import Project
+from .models import Group, Project
 
 
 # triggred when User object is created
@@ -23,16 +24,33 @@ def create_profile(sender, instance, created, **kwargs):
         for student in students:
             msg = MIMEMultipart()
 
+            page_student = reverse("choose_time",
+                                   args=[instance.id, student.id])
+
             message = f'Уважаемый {student.first_name}\n'\
                       f'Приглашаем на коммандный проект {instance.title}\n'\
                       f'Выберете время: '\
-                      f'http://127.0.0.1:8000/'\
-                      f'to_project/{instance.id}&{student.id}'
+                      f'http://{settings.ALLOWED_HOSTS[0]}{page_student}'
 
             msg['From'] = settings.SENDER_EMAIL_LOGIN
             msg['To'] = student.email
-            msg['Subject'] = f'Приглашение в командный проект "{instance.title}"'
+            msg['Subject'] =\
+                f'Приглашение в командный проект "{instance.title}"'
 
             msg.attach(MIMEText(message, 'plain'))
 
             smtpObj.send_message(msg)
+
+
+@receiver(post_save, sender=Group)
+def create_group(sender, instance, created, **kwargs):
+    if (not created) and\
+       (not instance.trello_url) and\
+       (not instance.telegram_chat_id) and\
+       (instance.is_complete):
+        print(f'-=-=-=-=-=-=-=-=-Send {instance.pm.name} to email PM: {instance.pm.email}')
+    elif (not created) and\
+         (instance.trello_url) and\
+         (instance.telegram_chat_id) and\
+         (instance.is_complete):
+        print(f'-=-=-=-=-=-=-=-=-Send students email: {instance.students}')
