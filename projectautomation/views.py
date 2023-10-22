ff276b8d9f20ca3320266971a6eaec86fafaf3a6
igorkorobko <nekto.korobko@gmail.com>
2023-10-22 10:57:46 +0300

    add feature to add student to exist group

--- projectautomation/views.py
+++ projectautomation/views.py
@@ -1,41 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 
 from .forms import ChooseTimeForm
-from .models import Student
-
-ERRORS = [
-    'Все группы заняты.',
-    'Этот временной слот уже скомплектован.',
-    'Может еще какая ошибка ))'
-]
+from .models import Student, Project, Group, PM, TimeSlot
 
 
-def show_time_slots(request, proj_id, user_id, error_id=None):
+def show_time_slots(request, project_id, user_id):
     try:
         student = Student.objects.get(id=user_id)
-    except Student.DoesNotExist:
+        project = Project.objects.get(id=project_id)
+    except Student.DoesNotExist or Project.DoesNotExist:
         raise Http404('Page Not Found')
     else:
         if request.method == 'POST':
+            print('request.POST', request.POST)
             form = ChooseTimeForm(request.POST)
+            print(form.fields)
+            print(form.errors)
             if form.is_valid():
                 best_time_slots = form.cleaned_data.get('best_time_slots')
-
-                student.best_time_slots.set(best_time_slots)
-
+                find_group = Group.objects.get(time_slot_id=best_time_slots)
+                if find_group:
+                    group = Group.objects.get(find_group)
+                    group.students.add(student)
+                else:
+                    Group.objects.create(student)
                 return HttpResponseRedirect('/thanks/')
 
-        form = ChooseTimeForm()
-        context = {'proj_id': proj_id,
-                   'user_id': user_id,
-                   'form': form,
-                   'name': student.first_name}
-
-        if error_id:
-            context['error'] = ERRORS[error_id - 1]
-
-        return render(request, 'index.html', context=context)
+        time_slots = TimeSlot.objects.all()
+        best_time_slots = Group.objects.filter(is_complete=1).values("time_slot_id")
+        unavailable_slots = []
+        for slots in best_time_slots:
+            unavailable_slots.append(slots['time_slot_id'])
+        time_slots = TimeSlot.objects.exclude(id__in=unavailable_slots)
+        return render(request, 'index.html',
+                      context={
+                          'user_id': user_id,
+                          'project_id': project_id,
+                          'available_slots': time_slots,
+                          'name': student.first_name
+                      })
 
 
 def show_thanks(request):
