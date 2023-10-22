15d4a13385873392c9f344890dece2796a819d61
igorkorobko <nekto.korobko@gmail.com>
2023-10-22 10:56:23 +0300

    rename fields in Group model

--- projectautomation/admin.py
+++ projectautomation/admin.py
@@ -13,8 +13,6 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = (
         'first_name', 'last_name', 'email', 'level')
 
-    list_filter = ('level',)
-
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
@@ -25,13 +23,12 @@ class ProjectAdmin(admin.ModelAdmin):
 class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ('start_time',)
 
-    ordering = ('start_time', )
-
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'description', 'time_slot', 'pm', 'trello_id', 'is_complete')
+        'title', 'description', 'time_slot', 'pm', 'trello_url', 'telegram_chat_id', 'is_complete'
+    )
 
 
 @admin.register(Levels)
